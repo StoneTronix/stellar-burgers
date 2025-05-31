f676963ac820674018de504a7b12f33e0585900e
StoneTronix <mileenkov.r.i@gmail.com>
2025-05-31 21:57:19 +0300

    feat: add protected route

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -12,6 +12,7 @@ import {
   ProfileOrders,
   NotFound404
 } from '@pages';
+import { ProtectedRoute } from '@components';
 
 import styles from './app.module.css';
 import '../../index.css';
@@ -22,19 +23,39 @@ const App = () => (
     <Routes>
       <Route path='/' element={<ConstructorPage />} />
       <Route path='/feed' element={<Feed />} />
-      <Route path='/login' element={<Login />} />
+      <Route
+        path='/login'
+        element={
+          <ProtectedRoute type='login'>
+            <Login />
+          </ProtectedRoute>
+        }
+      />
       <Route path='/register' element={<Register />} />
       <Route path='/forgot-password' element={<ForgotPassword />} />
       <Route path='/reset-password' element={<ResetPassword />} />
-      <Route path='/profile' element={<Profile />} />
-      <Route path='/profile/orders' element={<ProfileOrders />} />
+      <Route
+        path='/profile'
+        element={
+          <ProtectedRoute type='protected'>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile/orders'
+        element={
+          <ProtectedRoute type='protected'>
+            <ProfileOrders />
+          </ProtectedRoute>
+        }
+      />
       <Route path='*' element={<NotFound404 />} />
 
       <Route path='/ingredients/:id' element={<ConstructorPage />} />
       <Route path='/feed/:number' element={<Feed />} />
+
       <Route path='/profile/orders/:number' element={<ProfileOrders />} />
-    </Routes>
-    <Routes>
       <Route
         path='/feed/:number'
         element={
